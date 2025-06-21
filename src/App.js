19c221bcbf40cff26ac4a826e52e02c483593d8e
1sakib <1sakib.uddin@gmail.com>
2025-06-21 16:57:01 -0400

    Added MongoDB Database

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [samples, setSamples] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5050/api/samples')
+      .then(res => res.json())
+      .then(data => setSamples(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MongoDB Sample Data:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {samples.map((item, idx) => (
+            <li key={idx}>{item.name}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
